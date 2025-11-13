9568705f7c80772d1a3e2fb5ae22d1d6d2773c9b
IcantAffordSynapse <88977097+IcantAffordSynapse@users.noreply.github.com>
2025-11-13 17:03:51 +1100

    Create gamesList.lua

---
+++ gamesList.lua
@@ -0,0 +1,24 @@
+local games = {
+  RoCitizens = {
+    GameName = "RoCitizens",
+    Status = "🟢"
+  },
+  BuildAZoo = {
+    GameName = "Build a Zoo",
+    Status = "🟢"
+  },
+  TOH = {
+    GameName = "Tower of Hell",
+    Status = "🟡"
+  },
+  PoolClassic = {
+    GameName = "8 Ball Pool Classic",
+    Status = "🟢"
+  },
+  MVS = {
+    GameName = "Murderers vs Sheriff",
+    Status = "🟢"
+  },
+}
+
+return games
